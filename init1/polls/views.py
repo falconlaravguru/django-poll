98d54e6a8d70c894dd0e40a0aeb306911349b7a7
Hawk <hawk@localhost.localdomain>
2017-11-23 15:54:34 -0800

    Fill out Vote()

--- init1/polls/views.py
+++ init1/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
-from .models import Question
+from .models import Question,Choice
 
 def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5]
@@ -20,4 +20,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
