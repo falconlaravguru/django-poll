c28b977d0074775ec35600e8929284f1380cd37e
Hawk <hawk@localhost.localdomain>
2017-11-24 09:18:29 -0800

    chore; Add new test

--- init1/polls/tests.py
+++ init1/polls/tests.py
@@ -7,6 +7,15 @@ from polls.models import Question
 
 class QuestionModelTest(TestCase):
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
