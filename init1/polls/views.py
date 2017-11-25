76bf3c97aff6bf6f07ab7e159cd5d571b777d3db
hawksuperguru <falcon.laravguru@yandex.com>
2017-11-24 16:56:23 -0800

    chore; bug-fix - inject timezone

--- init1/polls/views.py
+++ init1/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.views import generic
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question,Choice
 
