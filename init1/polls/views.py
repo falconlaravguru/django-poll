c9a21cba1f2edea2e161040b9f900edb90f2bb9a
hawksuperguru <falcon.laravguru@yandex.com>
2018-01-20 08:52:13 -0800

    Completed CRUD

--- init1/polls/views.py
+++ init1/polls/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.views import generic
 from django.urls import reverse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from polls.serializers import QuestionSerializer
 
 from .models import Question,Choice
@@ -44,6 +47,50 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all().order_by('-pub_date')
     serializer_class = QuestionSerializer
 
+@csrf_exempt
+def question_list(request):
+    """
+    List all code questions, or create a new question.
+    """
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = QuestionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def question_detail(request, pk):
+    """
+    Retrieve, update or delete a question.
+    """
+    print(pk)
+    try:
+        question = Question.objects.get(pk=pk)
+    except Question.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = QuestionSerializer(question)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = QuestionSerializer(question, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        question.delete()
+        return HttpResponse(status=204)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
