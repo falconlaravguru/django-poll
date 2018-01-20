2210bd5cdb7265acfffc60897888fafbf422d194
hawksuperguru <falcon.laravguru@yandex.com>
2018-01-20 08:52:56 -0800

    Modification of Base Serializer

--- init1/polls/serializers.py
+++ init1/polls/serializers.py
@@ -1,7 +1,8 @@
 from polls.models import Question
 from rest_framework import serializers
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Question
         fields = ( 'question_text', 'pub_date' )
